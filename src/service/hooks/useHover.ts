27fed53ddac53a591e6d1f2bc5eb73c91e67e179
saxumcordis <victor123952@gmail.com>
2021-05-10 23:28:21 +0300

    add hook useHover

---
+++ src/service/hooks/useHover.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect, RefObject } from "react";
+
+export const useHover = <T extends HTMLElement = HTMLElement>(
+  elementRef: RefObject<T>
+): boolean => {
+  const [value, setValue] = useState<boolean>(false);
+
+  const handleMouseEnter = () => setValue(true);
+  const handleMouseLeave = () => setValue(false);
+
+  useEffect(() => {
+    const node = elementRef?.current;
+
+    if (node) {
+      node.addEventListener("mouseenter", handleMouseEnter);
+      node.addEventListener("mouseleave", handleMouseLeave);
+
+      return () => {
+        node.removeEventListener("mouseenter", handleMouseEnter);
+        node.removeEventListener("mouseleave", handleMouseLeave);
+      };
+    }
+  }, [elementRef]);
+
+  return value;
+};
