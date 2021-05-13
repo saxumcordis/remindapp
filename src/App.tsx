85ef4682d11a999f745c190fa68f77b2930eda52
saxumcordis <victor123952@gmail.com>
2021-05-14 00:50:54 +0300

    implement redirecting

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./App.css";
-import { Menu } from "./components/Menu";
 import { Container } from "./components/Container";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Dashboard } from "./components/Dashboard";
+import { MemoPage } from "./pages/MemoPage";
+import { NotFound } from "./components/NotFound";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Container>
-        <Menu />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/memo/:id" component={MemoPage} />
+          <Route component={NotFound} />
+        </Switch>
       </Container>
     </BrowserRouter>
   );
