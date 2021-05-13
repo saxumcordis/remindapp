a1cfff217069ce93162ab2c52e69e6e6d9b7729f
saxumcordis <victor123952@gmail.com>
2021-05-13 23:29:40 +0300

    add browser router

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import { Menu } from "./components/Menu";
 import { Container } from "./components/Container";
+import {BrowserRouter} from "react-router-dom";
 
 const App = () => {
   return (
+      <BrowserRouter>
     <Container>
       <Menu />
     </Container>
+      </BrowserRouter>
   );
 };
 
