1d62617893b2933e71e2fbb5e56286c42edafe32
saxumcordis <victor123952@gmail.com>
2021-05-14 00:08:05 +0300

    use dashboard component

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ThemeContainer } from "../../ui/Theme";
-
+import { Dashboard } from "../Dashboard";
 
 import styles from "./Container.module.scss";
 
@@ -8,6 +8,7 @@ export const Container: React.FC = ({ children }) => (
   <ThemeContainer>
     <div className={styles.background}>
       <div className={styles.children}>{children}</div>
+      <Dashboard />
     </div>
   </ThemeContainer>
 );
