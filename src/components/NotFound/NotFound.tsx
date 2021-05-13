66be3ed183a86d7fe2c28ba52c9dd17e6e16d1ad
saxumcordis <victor123952@gmail.com>
2021-05-14 00:48:48 +0300

    create notfound component

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from "./NotFound.module.scss";
+
+type NotFoundProps = {
+    message?: string;
+}
+
+export const NotFound: React.FC<NotFoundProps> = ({message}) => {
+    return <div className={styles.container}>
+        <span>Oops, {message} not found</span>
+    </div>
+};
