279c416bc7140d56f54e93b0382d77d46f244665
saxumcordis <victor123952@gmail.com>
2021-05-13 23:30:45 +0300

    fix

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { ThemeContainer } from "../../ui/Theme";
 
+
 import styles from "./Container.module.scss";
 
 export const Container: React.FC = ({ children }) => (
   <ThemeContainer>
     <div className={styles.background}>
-      <header className={styles.header}></header>
       <div className={styles.children}>{children}</div>
     </div>
   </ThemeContainer>
