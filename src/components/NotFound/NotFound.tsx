9ec60e0f12c7f757238a53d811c3e0248a3f10eb
saxumcordis <victor123952@gmail.com>
2021-05-14 00:49:58 +0300

    prettier fix

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 import styles from "./NotFound.module.scss";
 
 type NotFoundProps = {
-    message?: string;
-}
+  message?: string;
+};
 
-export const NotFound: React.FC<NotFoundProps> = ({message}) => {
-    return <div className={styles.container}>
-        <span>Oops, {message} not found</span>
+export const NotFound: React.FC<NotFoundProps> = ({ message }) => {
+  return (
+    <div className={styles.container}>
+      <span>Oops, {message} not found</span>
     </div>
+  );
 };
