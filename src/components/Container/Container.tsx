561958f17592dd82c750f4a6577a9ca0c451576f
saxumcordis <victor123952@gmail.com>
2021-05-17 01:45:52 +0300

    init header

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { ThemeContainer } from "../../ui/Theme";
 import { Menu } from "../Menu";
+import { Link } from "react-router-dom";
 
 import styles from "./Container.module.scss";
+import { Home } from "./Home";
 
 export const Container: React.FC = ({ children }) => (
   <ThemeContainer>
     <div className={styles.background}>
+      <div className={styles.header}>
+        <Link to="/">
+          <span>
+            REM
+            <Home className={styles.icon} />
+            IND
+          </span>
+        </Link>
+      </div>
       <div className={styles.children}>{children}</div>
       <Menu />
     </div>
