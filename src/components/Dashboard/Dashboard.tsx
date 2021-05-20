bf33fff5182b2fd05565aabe681824c6ffa54227
saxumcordis <victor123952@gmail.com>
2021-05-21 00:55:35 +0300

    implement masonry component

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { useMemoCards } from "../../service/memoCards/useMemoCards";
+import { MemoCard } from "../MemoCard";
+
+import styles from "./Dashboard.module.scss";
+import Masonry from "react-masonry-css";
 
 export const Dashboard: React.FC = () => {
-  return <>Hello</>;
+  const { memoCards } = useMemoCards();
+
+  const breakpointCols = {
+    default: 4,
+    1100: 4,
+    900: 3,
+    600: 2,
+    400: 1,
+  };
+
+  return (
+    <Masonry
+      className={styles.dashboard}
+      columnClassName={styles.column}
+      breakpointCols={breakpointCols}
+    >
+      {memoCards.map((memoCard) => (
+        <MemoCard memoCard={memoCard} />
+      ))}
+    </Masonry>
+  );
 };
