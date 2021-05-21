263d4c4debd00461e5603501b9b7d14a1a16e61e
saxumcordis <victor123952@gmail.com>
2021-05-21 19:36:37 +0300

    add new funcs

--- src/service/memoCards/service.ts
+++ src/service/memoCards/service.ts
@@ -1,5 +1,12 @@
-import { equals, filter, where } from "ramda";
+import { equals, filter, where, find, propEq } from "ramda";
+import { TMemo } from "../../types";
 
 export const getPinnedMemoCards = filter(where({ pinned: equals(true) }));
 
 export const getUnPinnedMemoCards = filter(where({ pinned: equals(false) }));
+
+export const sortByPinned = (memoCards: TMemo[]) =>
+  getPinnedMemoCards(memoCards).concat(getUnPinnedMemoCards(memoCards));
+
+export const getMemoCardById = (id: string, memoCards: TMemo[]) =>
+  find(propEq("id", id), memoCards);
