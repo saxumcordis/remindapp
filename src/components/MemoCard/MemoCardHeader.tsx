b4977422c45498fa5907854f847f71b81199de89
saxumcordis <victor123952@gmail.com>
2021-05-23 00:53:10 +0300

    fix default click, add date icon

--- src/components/MemoCard/MemoCardHeader.tsx
+++ src/components/MemoCard/MemoCardHeader.tsx
@@ -8,6 +8,8 @@ import { useMemoCards } from "../../service/memoCards/useMemoCards";
 import { Pinned } from "../../ui/Icons/Pinned";
 import { Deadline } from "../../ui/Icons/Deadline";
 import { Priority } from "../../ui/Icons/Priority";
+import { DateSort } from "../../ui/Icons/DateSort";
+import { EMemoPriorityNames } from "../../service/memoCards/service";
 
 type MemoCardHeaderProps = {
   id: string;
@@ -33,21 +35,31 @@ export const MemoCardHeader: React.FC<MemoCardHeaderProps> = ({
   };
   const handlePinMemoCard = () => {
     pinMemoCard(!pinned, id);
-  }
+  };
 
   return (
     <div className={styles.header}>
       <div className={styles.title}>
         <Input
+          onClick={(e) => e.preventDefault()}
           autoFocus={activeMemoCard?.id === id}
           className={styles.inputTitle}
           defaultValue={title}
           onChange={handleNewName}
         />
-        <span className={styles.date}>{createdTs?.split(",")[0]}</span>
+        <span className={styles.date}>
+          <DateSort className={styles.dateIcon} />
+          {createdTs?.split(",")[0]}
+        </span>
       </div>
-      <div className={styles.controls}>
-        <Priority priority={priority} />
+      <div
+        className={styles.controls}
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+        }}
+      >
+        <Priority priority={EMemoPriorityNames[priority]} />
         <Deadline deadline={`deadline_${Boolean(deadline)}`} />
         <Pinned
           className={pinned ? styles.pinned : ""}
