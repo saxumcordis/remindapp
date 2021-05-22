09fe9ab7a73a4cef2ddf0a60275a9a6dc8c26918
saxumcordis <victor123952@gmail.com>
2021-05-23 00:59:11 +0300

    divide pinned with unpinned memocards. use filtering

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -4,9 +4,22 @@ import { MemoCard } from "../MemoCard";
 
 import styles from "./Dashboard.module.scss";
 import Masonry from "react-masonry-css";
+import {
+  getPinnedMemoCards,
+  getUnPinnedMemoCards,
+  sortByField,
+  sortByOrder,
+} from "../../service/memoCards/service";
+import { useFiltering } from "../../service/memoCards/useFiltering";
 
 export const Dashboard: React.FC = () => {
   const { memoCards } = useMemoCards();
+  const { filter, filterOrder } = useFiltering();
+
+  const [pinnedMemoCards, unPinnedMemoCards] = [
+    getPinnedMemoCards(memoCards),
+    getUnPinnedMemoCards(memoCards),
+  ].map((e) => sortByOrder(sortByField(e, filter), filterOrder));
 
   const breakpointCols = {
     default: 4,
@@ -17,14 +30,25 @@ export const Dashboard: React.FC = () => {
   };
 
   return (
-    <Masonry
-      className={styles.dashboard}
-      columnClassName={styles.column}
-      breakpointCols={breakpointCols}
-    >
-      {memoCards.map((memoCard) => (
-        <MemoCard memoCard={memoCard} />
-      ))}
-    </Masonry>
+    <>
+      <Masonry
+        className={styles.dashboard}
+        columnClassName={styles.column}
+        breakpointCols={breakpointCols}
+      >
+        {pinnedMemoCards.map((memoCard) => (
+          <MemoCard key={memoCard.id} memoCard={memoCard} />
+        ))}
+      </Masonry>
+      <Masonry
+        className={styles.dashboard}
+        columnClassName={styles.column}
+        breakpointCols={breakpointCols}
+      >
+        {unPinnedMemoCards.map((memoCard) => (
+          <MemoCard key={memoCard.id} memoCard={memoCard} />
+        ))}
+      </Masonry>
+    </>
   );
 };
