44f9e0c1f0dd908914fb90ef7106aa6de65d512e
saxumcordis <victor123952@gmail.com>
2021-06-14 23:16:35 +0300

    add content func

--- src/service/content/content.ts
+++ src/service/content/content.ts
@@ -40,9 +40,32 @@ export const isValidContent: (content?: TContent) => boolean = anyPass([
   isValidTextContent,
 ]);
 
-export const getContentType: (
-  content?: TContent
-) => EContent | undefined = cond([
-  [isTextContent, always(EContent.String)],
-  [isListContent, always(EContent.List)],
-]);
+export const getContentType: (content?: TContent) => EContent | undefined =
+  cond([
+    [isTextContent, always(EContent.String)],
+    [isListContent, always(EContent.List)],
+  ]);
+
+export const listContentFromString = (content?: string): TContentList => {
+  if (!content) return [{ text: "", checked: false }];
+  return content
+    .split("\n")
+    .filter((e) => e)
+    .map((e) => ({ text: e.trim(), checked: false }));
+};
+
+export const stringContentFromList = (content?: TContentList): string => {
+  if (!content?.length) return "";
+  return content.map((e) => e.text).join("\n");
+};
+
+export const switchContent = (newType: EContent, content?: TContent) => {
+  switch (newType) {
+    case EContent.String:
+      return stringContentFromList((content || []) as TContentList);
+    case EContent.List:
+      return listContentFromString((content || "") as string);
+    default:
+      return "";
+  }
+};
