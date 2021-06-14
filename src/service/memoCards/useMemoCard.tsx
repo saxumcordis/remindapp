b9c0e0663590c8d74af91691e424fe93e4510455
saxumcordis <victor123952@gmail.com>
2021-06-14 23:16:14 +0300

    add switch content func

--- src/service/memoCards/useMemoCard.tsx
+++ src/service/memoCards/useMemoCard.tsx
@@ -1,5 +1,5 @@
-import React, { createContext, useContext } from "react";
-import { EMemoPriority, TMemo } from "../../types";
+import React, { createContext, useCallback, useContext } from "react";
+import { EContent, EMemoPriority, TContent, TMemo } from "../../types";
 
 import omit from "lodash/omit";
 
@@ -7,11 +7,14 @@ import { NIL as NIL_UUID } from "uuid";
 import { useMemoCards } from "./useMemoCards";
 import { noop } from "../utils/noop";
 import { getMemoCardById } from "./service";
+import { switchContent } from "../content/content";
 
 type TMemoCardContext = {
   memoCard: TMemo | null;
   renameMemoCard: (newTitle: string, id?: string) => void;
   pinMemoCard: (pinned: boolean, id?: string) => void;
+  switchContentType: (newType: EContent, id?: string) => void;
+  saveContent: (newContent: TContent, id?: string) => void;
 };
 
 export const DEFAULT_MEMOCARD: TMemo = {
@@ -29,6 +32,8 @@ const DEFAULT_MEMOCARD_CONTEXT: TMemoCardContext = {
   memoCard: DEFAULT_MEMOCARD,
   renameMemoCard: noop,
   pinMemoCard: noop,
+  switchContentType: noop,
+  saveContent: noop,
 };
 
 const MemoCardContext = createContext<TMemoCardContext>(
@@ -39,26 +44,61 @@ export const useMemoCard = () => useContext(MemoCardContext);
 
 export const MemoCardContextProvider: React.FC = ({ children }) => {
   const { memoCards, activeMemoCard, updateMemoCard } = useMemoCards();
-  const renameMemoCard = (newTitle: string) => {
-    if (activeMemoCard) {
-      updateMemoCard({
-        ...{ ...omit(activeMemoCard, ["title"]), title: newTitle },
-      });
-    }
-  };
+  const renameMemoCard = useCallback(
+    (newTitle: string) => {
+      if (activeMemoCard) {
+        updateMemoCard({
+          ...{ ...omit(activeMemoCard, ["title"]), title: newTitle },
+        });
+      }
+    },
+    [activeMemoCard, updateMemoCard]
+  );
 
-  const pinMemoCard = (pinned: boolean, id?: string) => {
-    if (id) {
-      updateMemoCard({
-        ...{ ...omit(getMemoCardById(id!, memoCards), ["pinned"]), pinned },
-      });
-    }
-  };
+  const pinMemoCard = useCallback(
+    (pinned: boolean, id?: string) => {
+      if (id) {
+        updateMemoCard({
+          ...{ ...omit(getMemoCardById(id!, memoCards), ["pinned"]), pinned },
+        });
+      }
+    },
+    [memoCards, updateMemoCard]
+  );
+
+  const switchContentType = useCallback(
+    (newType: EContent, id?: string) => {
+      if (id) {
+        const memoCard = getMemoCardById(id!, memoCards);
+        const content = switchContent(newType, memoCard?.content);
+        updateMemoCard({
+          ...{ ...omit(memoCard, ["content"]), content },
+        });
+      }
+    },
+    [memoCards, updateMemoCard]
+  );
+
+  const saveContent = useCallback(
+    (newContent: TContent, id?: string) => {
+      if (id) {
+        updateMemoCard({
+          ...{
+            ...omit(getMemoCardById(id, memoCards), ["content"]),
+            content: newContent,
+          },
+        });
+      }
+    },
+    [memoCards, updateMemoCard]
+  );
 
   const value = {
     memoCard: activeMemoCard,
     renameMemoCard,
     pinMemoCard,
+    switchContentType,
+    saveContent,
   };
 
   return (
