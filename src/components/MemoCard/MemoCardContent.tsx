b0eceb0267b26ca82e7645db3f1fcb9632512919
saxumcordis <victor123952@gmail.com>
2021-06-23 00:58:19 +0300

    implement content list item to memocard content

--- src/components/MemoCard/MemoCardContent.tsx
+++ src/components/MemoCard/MemoCardContent.tsx
@@ -1,20 +1,29 @@
 import React from "react";
-import { EContent, TContent } from "../../types";
+import { EContent, TContent, TContentList } from "../../types";
+import { ContentListItem } from "../../pages/MemoPage/MemoPageContent/MemoPageContent";
 import { getContentType, isValidContent } from "../../service/content/content";
+import { v4 as uuidv4 } from "uuid";
 
 import styles from "./MemoCard.module.scss";
 
-type TMemoCardContent = { content?: TContent; expanded?: boolean };
+type TMemoCardContent = { content?: TContent; expanded?: boolean; id: string };
 
 export const MemoCardContent: React.FC<TMemoCardContent> = ({
   content,
   expanded,
+  id,
 }) => {
   const contentType = getContentType(content);
   if (isValidContent(content)) {
     const ContentWrapper: React.FC = ({ children }) => {
       return (
-        <div className={!expanded ? styles.contentOverflow : styles.content}>
+        <div
+          className={
+            !expanded && contentType === EContent.String
+              ? styles.contentOverflow
+              : styles.content
+          }
+        >
           {children}
         </div>
       );
@@ -26,7 +35,24 @@ export const MemoCardContent: React.FC<TMemoCardContent> = ({
         </ContentWrapper>
       );
     else if (contentType === EContent.List)
-      return <ContentWrapper>LIST</ContentWrapper>;
+      return (
+        <ContentWrapper>
+          <>
+            {(content as TContentList)
+              .filter((e) => e.text?.trim())
+              .slice(0, expanded ? content?.length : 2)
+              .map((listItem, index) => (
+                <ContentListItem
+                  key={uuidv4()}
+                  index={index}
+                  id={id}
+                  {...listItem}
+                  readOnly={true}
+                />
+              ))}
+          </>
+        </ContentWrapper>
+      );
   }
   return <></>;
 };
